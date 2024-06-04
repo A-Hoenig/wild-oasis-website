be38e89c79eed7f8bd49715d623f3de75b6949f1
Alex <147496294+A-Hoenig@users.noreply.github.com>
2024-06-04 12:57:26 +0200

    setup josefin font

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,14 @@ import Logo from "./_components/Logo";
 import Navigation from "./_components/Navigation";
 import "@/app/_styles/globals.css";
 
+//setup font import and configure font add to body with ${josefin.className}
+import { Josefin_Sans } from "next/font/google";
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+// font import end
+
 export const metadata = {
   title: {
     template: "%s The Wild Oasis",
@@ -14,7 +22,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+      >
         <header>
           <Logo />
         </header>
